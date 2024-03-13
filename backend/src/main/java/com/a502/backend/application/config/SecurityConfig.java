21c7a9a8e65ee956ac89cb7ad6c78c5e0a936e16
김라연 <fkdusrh@naver.com>
2024-03-13 16:00:53 +0900

    feat: security 필터 설정 파일 추가

---
+++ backend/src/main/java/com/a502/backend/application/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.a502.backend.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  /*  @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/user/login").permitAll() // 모든 사용자에게 접근을 허용
+                        .anyRequest().authenticated()
+                        .csrf().disable
+                );
+        return http.build();
+    }*/
+
+}

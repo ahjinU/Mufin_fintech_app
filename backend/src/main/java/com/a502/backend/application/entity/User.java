3f74a8481852a221bb199b78720b5ade159e6635
김라연 <fkdusrh@naver.com>
2024-03-13 13:05:46 +0900

    feat: 회원 entity 추가

---
+++ backend/src/main/java/com/a502/backend/application/entity/User.java
@@ -0,0 +1,88 @@
+package com.a502.backend.application.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "user_uuid")
+    private byte[] userUuid;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "address2")
+    private String address2;
+
+    @Column(name = "type")
+    private Integer type;
+
+    @Column(name = "telephone")
+    private String telephone;
+
+    @Column(name = "birth")
+    @Temporal(TemporalType.DATE)
+    private Date birth;
+
+    @Column(name = "failed")
+    private Short failed;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private User parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.REMOVE)
+    private List<User> childrens = new ArrayList<>();
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Account> accounts = new ArrayList<>();
+
+
+    @Builder
+    public User(int userId, byte[] userUuid, String name, String email, String password, String gender, String address, String address2, Integer type, String telephone, Date birth, Short failed, User parent, Boolean isDeleted) {
+        this.userId = userId;
+        this.userUuid = userUuid;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.address = address;
+        this.address2 = address2;
+        this.type = type;
+        this.telephone = telephone;
+        this.birth = birth;
+        this.failed = failed;
+        this.parent = parent;
+        this.isDeleted = isDeleted;
+    }
+}

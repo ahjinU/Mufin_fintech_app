91d0240970bbeb1854687d6cb9dddafe66deb3da
김라연 <fkdusrh@naver.com>
2024-03-13 15:58:58 +0900

    feat: 더치페이 Entity 추가

--- backend/src/main/java/com/a502/backend/application/entity/Dutch.java
+++ backend/src/main/java/com/a502/backend/application/entity/Dutch.java
@@ -1,4 +1,52 @@
 package com.a502.backend.application.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "dutchs")
 public class Dutch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dutch_id")
+    private Integer dutchId;
+
+    @Column(name = "dutch_uuid")
+    private byte[] dutchUuid;
+
+    @Column(name = "total_person")
+    private Integer totalPerson;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "received_persons_id")
+    private User receivedUser;
+
+    @Column(name = "received_persons")
+    private Integer receivedPersonsCnt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_detail_id")
+    private AccountDetail accountDetail;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Builder
+
+    public Dutch(Integer dutchId, byte[] dutchUuid, Integer totalPerson, User receivedUser, Integer receivedPersonsCnt, AccountDetail accountDetail, Boolean isDeleted) {
+        this.dutchId = dutchId;
+        this.dutchUuid = dutchUuid;
+        this.totalPerson = totalPerson;
+        this.receivedUser = receivedUser;
+        this.receivedPersonsCnt = receivedPersonsCnt;
+        this.accountDetail = accountDetail;
+        this.isDeleted = isDeleted;
+    }
 }
--- backend/src/main/java/com/a502/backend/application/entity/DutchDetail.java
+++ backend/src/main/java/com/a502/backend/application/entity/DutchDetail.java
@@ -1,4 +1,41 @@
 package com.a502.backend.application.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "dutch_details")
 public class DutchDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dutch_detail_id")
+    private Integer dutchDetailId;
+
+    @Column(name = "dutch_detail_uuid")
+    private byte[] dutchDetailUuid;
+
+    @Column(name = "account_detail_id")
+    private Integer accountDetailId2;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dutch_id", referencedColumnName = "dutch_id")
+    private Dutch dutch;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Builder
+
+    public DutchDetail(Integer dutchDetailId, byte[] dutchDetailUuid, Integer accountDetailId2, Dutch dutch, Boolean isDeleted) {
+        this.dutchDetailId = dutchDetailId;
+        this.dutchDetailUuid = dutchDetailUuid;
+        this.accountDetailId2 = accountDetailId2;
+        this.dutch = dutch;
+        this.isDeleted = isDeleted;
+    }
 }

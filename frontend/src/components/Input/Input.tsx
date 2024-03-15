3186b96d934a96aa81abaae25caa92c52c4741af
윤진아 <chris2761@naver.com>
2024-03-15 14:11:19 +0900

    [#18]fix: Input, ComplexInput textCustomBlack 추가

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -3,7 +3,7 @@
 import { XCircleIcon } from '@heroicons/react/24/solid';
 import { useRef, useState } from 'react';
 
-interface InputProps{
+interface InputProps {
   width?: string;
   reset?: boolean;
   placeholder?: string;
@@ -11,10 +11,16 @@ interface InputProps{
   setValue?: Function;
 }
 
-export default function Input({ width , reset = true, placeholder, value, setValue, ...props } : InputProps ){
-
-  const [ inputPlaceholder, setInputPlaceholder ] = useState( placeholder || '' );
-  const [ inputValue, setInputValue ] = useState( value || '' );
+export default function Input({
+  width,
+  reset = true,
+  placeholder,
+  value,
+  setValue,
+  ...props
+}: InputProps) {
+  const [inputPlaceholder, setInputPlaceholder] = useState(placeholder || '');
+  const [inputValue, setInputValue] = useState(value || '');
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleReset = () => {
@@ -24,32 +30,33 @@ export default function Input({ width , reset = true, placeholder, value, setVal
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
-    setInputValue( newValue );
-    setValue && setValue( newValue );
+    setInputValue(newValue);
+    setValue && setValue(newValue);
   };
 
-  return(
+  return (
     <div
-      className= {`min-w-[16rem] h-[4.4rem] ${ width || 'w-full' } bg-custom-white border border-custom-medium-gray 
-      rounded-[0.8rem] p-[1.5rem] pr-[1rem]  border-[0.1rem]
+      className={`min-w-[16rem] h-[4.4rem] ${
+        width || 'w-full'
+      } bg-custom-white border-custom-medium-gray  border-[0.1rem] 
+      rounded-[0.8rem] p-[1.5rem] pr-[1rem] 
       focus:outline-none focus-within:border-custom-purple focus-within:ring-[0.05rem] focus-within:ring-custom-purple
-      text-[1.6rem] custom-semibold-text flex items-center justify-center`}
+      flex items-center justify-center`}
     >
-      <input 
-        className= 'w-full outline-none text-[1.6rem] custom-semibold-text'
-        { ...props }
-        placeholder = { inputPlaceholder }
-        value= { inputValue }
-        ref={ inputRef }
-        onChange= { handleChange }
-        onFocus= {() => setInputPlaceholder('')}
+      <input
+        className="w-full outline-none text-[1.6rem] custom-semibold-text text-custom-black"
+        {...props}
+        placeholder={inputPlaceholder}
+        value={inputValue}
+        ref={inputRef}
+        onChange={handleChange}
+        onFocus={() => setInputPlaceholder('')}
       />
-        {
-          reset && 
-          <button onClick= { handleReset }>
-            <XCircleIcon className= 'h-[2.4rem] w-[2.4rem] text-custom-light-gray hover:text-custom-medium-gray'/>
-          </button>
-        }
-      </div>
-  )
+      {reset && (
+        <button onClick={handleReset}>
+          <XCircleIcon className="h-[2.4rem] w-[2.4rem] text-custom-light-gray hover:text-custom-medium-gray" />
+        </button>
+      )}
+    </div>
+  );
 }

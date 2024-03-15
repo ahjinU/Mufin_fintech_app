a882d98051c87675bbbf6514c662de4f4d9dd129
jep1995 <jep1995@naver.com>
2024-03-15 14:54:40 +0900

    fix: user ìcntroller, reposer 수정

--- backend/src/main/java/com/a502/backend/application/controller/UserController.java
+++ backend/src/main/java/com/a502/backend/application/controller/UserController.java
@@ -3,6 +3,7 @@ package com.a502.backend.application.controller;
 import com.a502.backend.application.entity.User;
 import com.a502.backend.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,20 +19,18 @@ import java.util.Map;
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
-
 	private final UserService userService;
 
 	//유저 등록
 	@PostMapping("/login")
-	public ResponseEntity<?> loginUser(@RequestBody Integer userId) {
+	public ResponseEntity<?> loginUser(@RequestBody int id) {
 
 		System.out.println("나를 불렀나");
-		String email = userService.loginUser(userId);
+		String email = userService.loginUser(id);
 
 		Map<String, Object> result = new HashMap<String, Object>();
 		result.put("email", email);
 
 		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
 	}
-
 }

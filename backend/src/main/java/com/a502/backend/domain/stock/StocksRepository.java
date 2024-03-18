5ec0335fe08d2f2dbd6fd9bcbf34baeab2267137
jep1995 <jep1995@naver.com>
2024-03-18 09:52:30 +0900

    [#26] feat: stockrepository jpa상 ììrepository 상속

--- backend/src/main/java/com/a502/backend/domain/stock/StocksRepository.java
+++ backend/src/main/java/com/a502/backend/domain/stock/StocksRepository.java
@@ -1,7 +1,10 @@
 package com.a502.backend.domain.stock;
 
+import com.a502.backend.application.entity.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StocksRepository {
+public interface StocksRepository extends JpaRepository<Stock,Integer> {
+
 }

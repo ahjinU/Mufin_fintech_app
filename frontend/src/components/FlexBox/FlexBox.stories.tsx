3e13344a9493f3e9d10c5a64103cfa73c209f34c
jsm0726 <jsm0726@naver.com>
2024-03-18 17:34:02 +0900

    [#35] feat: 구분선이 있거나 없는 세로 방향 FlexBox 공통 컴포넌트 구현 및 스토리북 문서 작성

---
+++ frontend/src/components/FlexBox/FlexBox.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import FlexBox from './FlexBox';
+import OtherInfoElement from ./OtherInfoElement';
+
+const meta = {
+  title: 'Common/FlexBox',
+  component: FlexBox,
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: {
+    isDivided: {
+      description: '구분선이 있는지 boolean 값을 prop으로 전달합니다.',
+    },
+  },
+} satisfies Meta<typeof FlexBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Divided: Story = {
+  args: {
+    isDivided: true,
+    topChildren: <OtherInfoElement imageSrc='http://localhost:3000/images/icon-dollar.png' leftExplainText='화장품' leftHighlightText='씨제이올리브영 주식회사' state='DOWN' money='50,000 원'/>,
+    bottomChildren: <span>5천원 할인 받았다!</span>
+  },
+};

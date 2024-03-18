dee64ae5d2c900035a2c3e9cff56efddfc9cf470
윤진아 <chris2761@naver.com>
2024-03-18 15:44:59 +0900

    [#36]fix: ComplexInput ERROR, INFOM 내용 모두 받도록 수정

--- frontend/src/components/ComplexInput/ComplexInput.stories.ts
+++ frontend/src/components/ComplexInput/ComplexInput.stories.ts
@@ -8,32 +8,46 @@ const meta = {
   parameters: {},
   tags: ['autodocs'],
   argTypes: {
-   label: {
-    description: '소제목입니다.'
-   },
-   width: {
-    description: 'width를 지정할 수 있습니다'
-   },
-   children: {
-    description: '내부 컴포넌트들은 children으로 보내줍니다.'
-   },
-   errorMsg: {
-    description: 'errorMsg를 지정할 수 있습니다'
-   },
-   isError: {
-    description: '에러메세지 조건이라면 true로 지정해주면 됩니다. 기본 값은 false 입니다.'
-   },
+    label: {
+      description: '소제목입니다.',
+    },
+    width: {
+      description: 'width를 지정할 수 있습니다',
+    },
+    children: {
+      description: '내부 컴포넌트들은 children으로 보내줍니다.',
+    },
+    message: {
+      description: 'errorMsg 또는 InfomMsg를 지정할 수 있습니다',
+    },
+    isMsg: {
+      description:
+        '메세지 조건이라면 true로 지정해주면 됩니다. 기본 값은 false 입니다.',
+    },
+    mode: {
+      desciption:
+        '메세지의 타입입니다. ERROR와 INFORM와 메세지를 띄우지 않는 NONE이 있습니다',
+    },
   },
-} satisfies Meta<typeof ComplexInput
->;
+} satisfies Meta<typeof ComplexInput>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Main: Story = {
+export const ERROR: Story = {
   args: {
     label: '내용을 입력해주세요',
-    isError: true,
-    errorMsg: '틀렸어요',
+    isMsg: true,
+    message: '틀렸어요',
+    mode: 'ERROR',
+  },
+};
+
+export const INFORM: Story = {
+  args: {
+    label: '내용을 입력해주세요',
+    isMsg: true,
+    message: '10,000입금되요',
+    mode: 'INFORM',
   },
 };
--- frontend/src/components/ComplexInput/ComplexInput.tsx
+++ frontend/src/components/ComplexInput/ComplexInput.tsx
@@ -1,8 +1,9 @@
 interface ComplexInput {
   label: string;
   width?: string;
-  errorMsg?: string;
-  isError?: boolean;
+  message?: string;
+  isMsg?: boolean;
+  mode: 'ERROR' | 'INFORM' | 'NONE';
   children?: React.ReactElement;
 }
 
@@ -10,17 +11,22 @@ export default function ComplexInput({
   label,
   width,
   children,
-  errorMsg,
-  isError = false,
+  message,
+  isMsg = false,
+  mode,
   ...props
 }: ComplexInput) {
   return (
     <div className={`min-w-[16rem] ${width || 'w-full'}`} {...props}>
       <label className="custom-semibold-text text-custom-black">{label}</label>
       <div className="mt-2">{children}</div>
-      {errorMsg && isError && (
-        <p className="mt-1 custom-light-text text-custom-red text-[1.2rem] flex justify-end">
-          {errorMsg}
+      {message && isMsg && (
+        <p
+          className={`mt-1 custom-light-text ${
+            mode === 'ERROR' ? 'text-custom-red' : 'text-custom-dark-gray'
+          } text-[1.2rem] flex`}
+        >
+          {message}
         </p>
       )}
     </div>

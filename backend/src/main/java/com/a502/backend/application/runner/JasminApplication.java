3b556f88e8a22f8f9033b932f2fa44899dbe207b
slcloe <67732600+slcloe@users.noreply.github.com>
2024-03-19 15:28:29 +0900

    [#29] feat : initFacade Create

--- backend/src/main/java/com/a502/backend/application/runner/JasminApplication.java
+++ backend/src/main/java/com/a502/backend/application/runner/JasminApplication.java
@@ -1,2 +1,23 @@
-package com.a502.backend.application.runner;public class JasminApplication {
+package com.a502.backend.application.runner;
+
+import com.a502.backend.application.facade.InitFacade;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOError;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JasminApplication implements ApplicationRunner {
+    private final InitFacade initFacade;
+
+    @Override
+    public void run(ApplicationArguments args)throws IOException{
+        initFacade.run();
+    }
 }

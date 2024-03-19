303b27cf95a2b6148d67dfcf0b935119ac3a7005
김라연 <fkdusrh@naver.com>
2024-03-19 15:40:32 +0900

    [#23]feat: 머지 충돌 피하기 위한 커밋

--- backend/src/main/java/com/a502/backend/domain/user/UserService.java
+++ backend/src/main/java/com/a502/backend/domain/user/UserService.java
@@ -1,17 +1,29 @@
 package com.a502.backend.domain.user;
 
+import com.a502.backend.application.dto.LoginDto;
+import com.a502.backend.application.dto.UserDto;
+import com.a502.backend.application.entity.Authority;
 import com.a502.backend.application.entity.User;
 import com.a502.backend.global.error.BusinessException;
 import com.a502.backend.global.exception.ErrorCode;
+import io.lettuce.core.ScriptOutputType;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
     public User findById(int id){
         return userRepository.findById(id).orElseThrow(() -> BusinessException.of(ErrorCode.API_ERROR_USER_NOT_EXIST));
     }
@@ -21,4 +33,33 @@ public class UserService {
         return user.getEmail();
     }
 
+
+
+    @Transactional
+    public UUID signup(User user) {
+
+        return user.getUserUuid();
+    }
+
+    public User login(LoginDto loginUser) {
+
+        LoginDto regUser = new LoginDto("TEST","TEST");
+        User registUser = modelMapper.map(regUser,User.class);
+        Object ob = userRepository.save(registUser);
+
+        System.out.println(ob);
+        System.out.println("=====등록 테스트=====");
+
+        System.out.println("[service] email: "+loginUser.getEmail());
+
+        User findMember = userRepository.findByEmail(loginUser.getEmail())
+                .orElseThrow(() -> BusinessException.of(ErrorCode.API_ERROR_USER_NOT_EXIST));
+
+        if (!findMember.checkPassword(loginUser.getPassword())) {
+            throw BusinessException.of(ErrorCode.API_ERROR_USER_INCORRECT_PASSWORD);
+        }
+
+        return findMember;
+
+    }
 }

df38be9c1e722955912d54a44acb6459f9f1ca01
김라연 <fkdusrh@naver.com>
2024-03-20 17:50:42 +0900

    [#23]feat: 로그인 기능 관련 설정파일 추가

--- backend/src/main/java/com/a502/backend/application/config/SecurityConfig.java
+++ backend/src/main/java/com/a502/backend/application/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         // 해당 API에 대해서는 모든 요청을 허가
-                        .requestMatchers("user/signUp/child").authenticated()
+                        .requestMatchers("user/sign-up/child").authenticated()
                         // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
                         .anyRequest().permitAll())
 //                        // USER 권한이 있어야 요청할 수 있음

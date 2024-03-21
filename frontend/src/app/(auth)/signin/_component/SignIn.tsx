d03fcb9bef768abaef4ec39425e8cacde475b7d2
Seoan Yoon <earthdyoon@gmail.com>
2024-03-21 16:31:12 +0900

    [#47] feat: NextAuth.js 적용 및 설정

--- frontend/src/app/(auth)/signin/_component/SignIn.tsx
+++ frontend/src/app/(auth)/signin/_component/SignIn.tsx
@@ -2,7 +2,6 @@ import { ComplexInput, Input, NavText } from '@/components';
 import SignInButton from './SignInButton';
 
 export default function SignIn() {
-
   return (
     <div className="flex flex-col gap-[1rem]">
       <ComplexInput label="이메일" message="" mode="NONE">
@@ -17,13 +16,15 @@ export default function SignIn() {
         <Input placeholder="비밀번호를 입력해주세요" />
       </ComplexInput>
       <SignInButton />
-      <div className="flex justify-between">
-        <p className="custom-light-text">비밀번호를 잊으셨나요?</p>
-        <NavText link="/" text="비밀번호 찾기" />
-      </div>
-      <div className="flex justify-between">
-        <p className="custom-light-text">아직 회원이 아니신가요?</p>
-        <NavText link="/" text="회원가입" />
+      <div className='flex flex-col gap-[0.4rem]'>
+        <div className="flex justify-between">
+          <p className="custom-light-text">비밀번호를 잊으셨나요?</p>
+          <NavText link="/" text="비밀번호 찾기" />
+        </div>
+        <div className="flex justify-between">
+          <p className="custom-light-text">아직 회원이 아니신가요?</p>
+          <NavText link="/" text="회원가입" />
+        </div>
       </div>
     </div>
   );
--- frontend/src/app/(auth)/signin/_component/SignInButton.tsx
+++ frontend/src/app/(auth)/signin/_component/SignInButton.tsx
@@ -1,11 +1,13 @@
-'use client'
+'use client';
 
 import { Button } from '@/components';
 
 export default function LoginButton() {
+  const onSubmit = async () => {};
+
   return (
     <div className="my-[1rem]">
-      <Button label="로그인" mode="ACTIVE" />
+      <Button label="로그인" mode="ACTIVE" onClick={onSubmit} />
     </div>
   );
 }

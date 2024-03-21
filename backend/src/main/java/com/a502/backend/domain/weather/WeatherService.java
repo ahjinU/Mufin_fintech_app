1181819a3cb2d9dee2e3dc45abbdc555f0fcd6ac
slcloe <67732600+slcloe@users.noreply.github.com>
2024-03-21 13:43:46 +0900

    [#50] feat : add Weather Calculator Scheduler

--- backend/src/main/java/com/a502/backend/domain/weather/WeatherService.java
+++ backend/src/main/java/com/a502/backend/domain/weather/WeatherService.java
@@ -3,6 +3,7 @@ package com.a502.backend.domain.weather;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -16,84 +17,68 @@ import java.net.URL;
 public class WeatherService{
 
     @Value("${weatherKey}")
-    static String apiKey;
-    public void weatherApi() throws Exception {
+    private String apiKey;
+    public Weather weatherApi() throws Exception {
         URL url = null;
         String readLine = null;
         StringBuilder buffer = null;
         BufferedReader bufferedReader = null;
         BufferedWriter bufferedWriter = null;
         HttpURLConnection urlConnection = null;
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=37.56667&lon=126.97806&appid=" + apiKey + "&units=metric";
 
-        int connTimeout = 5000;
-        int readTimeout = 3000;
-        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=37.56667&lon=126.97806&appid=e13e21fafd8d125d652d773982d07080"+"&units=metric";
-//        String apiUrll = "https://api.openweathermap.org/data/2.5/weather?lat=37.56667&lon=126.97806&appid="+apiKey;
-        log.info("apiKey={}",apiKey);
-        try
-        {
-            url = new URL(apiUrl);
-            urlConnection = (HttpURLConnection)url.openConnection();
-            urlConnection.setConnectTimeout(5000);
-            urlConnection.setReadTimeout(3000);
-            urlConnection.setRequestProperty("Accept", "application/json;");
+        url = new URL(apiUrl);
+        urlConnection = (HttpURLConnection)url.openConnection();
+        urlConnection.setConnectTimeout(5000);
+        urlConnection.setReadTimeout(3000);
+        urlConnection.setRequestProperty("Accept", "application/json;");
 
-            buffer = new StringBuilder();
-            if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
-                while((readLine = bufferedReader.readLine()) != null) {
-                    buffer.append(readLine).append("\n");
-                }
-            }
-            else {
-                buffer.append("code : ");
-                buffer.append(urlConnection.getResponseCode()).append("\n");
-                buffer.append("message : ");
-                buffer.append(urlConnection.getResponseMessage()).append("\n");
-            }
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            try {
-                if (bufferedWriter != null) { bufferedWriter.close(); }
-                if (bufferedReader != null) { bufferedReader.close(); }
-            }
-            catch(Exception ex) {
-                ex.printStackTrace();
+        buffer = new StringBuilder();
+        if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+            while((readLine = bufferedReader.readLine()) != null) {
+                buffer.append(readLine).append("\n");
             }
         }
 
+        if (bufferedWriter != null) bufferedWriter.close();
+        if (bufferedReader != null) bufferedReader.close();
 
-        System.out.println("결과 : " + buffer.toString());
-        JSONObject jsonObject = new JSONObject(buffer.toString());
+        return jsonParsing(buffer.toString());
+    }
 
+    public Weather jsonParsing(String json){
+        JSONObject jsonObject = new JSONObject(json);
         JSONObject main = jsonObject.getJSONObject("main");
         JSONObject windObject = jsonObject.getJSONObject("wind");
         JSONObject cloudsObject = jsonObject.getJSONObject("clouds");
-        int humidity = main.getInt("humidity");
-        double wind = windObject.getDouble("speed");
-        int cloud = cloudsObject.getInt("all");
+
+        int humidity;
+        int cloud;
+        double wind;
+        double rain = 0;
+        double snow = 0;
+
+        humidity = main.getInt("humidity");
+        wind = windObject.getDouble("speed");
+        cloud = cloudsObject.getInt("all");
 
         if (jsonObject.has("rain")){
             JSONObject rainObject = jsonObject.getJSONObject("rain");
-            String rain = rainObject.getString("1h");
-            log.info("rain={}",rain);
+            rain = rainObject.getDouble("1h");
         }
 
         if (jsonObject.has("snow")){
             JSONObject snowObject = jsonObject.getJSONObject("snow");
-            String snow = snowObject.getString("1h");
-            log.info("snow={}",snow);
+            snow = snowObject.getDouble("1h");
         }
-        log.info("humidity={}",humidity);
-        log.info("wind_speed={}",wind);
-        log.info("cloudvolume={}",cloud);
 
+        return Weather.builder()
+                .humidity(humidity)
+                .cloud(cloud)
+                .wind(wind)
+                .rain(rain)
+                .snow(snow)
+                .build();
     }
-
-    public void run() {
-        log.info("lilrumlelrul");
-    }
-
-
 }
--- backend/src/main/java/com/a502/backend/global/code/StockCode.java
+++ backend/src/main/java/com/a502/backend/global/code/StockCode.java
@@ -1,11 +1,8 @@
 package com.a502.backend.global.code;
 
 public class StockCode {
-    public static final int STOCK_STATUS_DONE = 3;
-    public static final int STOCK_STATUS_CANCLE = 2;
-    public static final int STOCK_STATUS_TRANS= 1;
-    public static final int PARKING_TRANSCODE_INTEREST = 100;
-    public static final int PARKING_TRANSCODE_BUY = 101;
-    public static final int PARKING_TRANSCODE_SELL = 102;
+    public static final double STOCK_PRICE_LV1 = 0.1;
+    public static final double STOCK_PRICE_LV2 = 0.15;
+    public static final double STOCK_PRICE_LV3 = 0.2;
 
 }

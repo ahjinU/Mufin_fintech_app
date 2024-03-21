b00f668a8a9712203e3a625db26a20e77f86f455
slcloe <67732600+slcloe@users.noreply.github.com>
2024-03-21 15:56:48 +0900

    [#50] feat: add RedisTest

---
+++ backend/src/main/java/com/a502/backend/global/config/RedisTest.java
@@ -0,0 +1,22 @@
+package com.a502.backend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Connection;
+
+@Component
+@RequiredArgsConstructor
+public class RedisTest{
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public String createRecord(int id) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set("hanseul", "rayeon");
+        return "plz";
+    }
+}
+

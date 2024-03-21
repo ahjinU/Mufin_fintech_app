75aa68408e8937445df27b809eb769833b7b0bbd
slcloe <67732600+slcloe@users.noreply.github.com>
2024-03-21 09:41:44 +0900

    [#50] feat: add Weather API link

---
+++ backend/src/main/java/com/a502/backend/domain/weather/WeatherService.java
@@ -0,0 +1,82 @@
+package com.a502.backend.domain.weather;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
+public class WeatherService{
+
+    @Value("${weatherKey}")
+    static String apiKey;
+    public static void weatherApi() throws Exception {
+        URL url = null;
+        String readLine = null;
+        StringBuilder buffer = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        HttpURLConnection urlConnection = null;
+
+        int connTimeout = 5000;
+        int readTimeout = 3000;
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=37.56667&lon=126.97806&appid=e13e21fafd8d125d652d773982d07080"+"&units=metric";
+//        String apiUrll = "https://api.openweathermap.org/data/2.5/weather?lat=37.56667&lon=126.97806&appid="+apiKey;
+        System.out.println("apiKey" + apiKey);
+//        System.out.println(apiUrl);
+//        System.out.println(apiUrll);
+        try
+        {
+            url = new URL(apiUrl);
+            urlConnection = (HttpURLConnection)url.openConnection();
+            urlConnection.setConnectTimeout(connTimeout);
+            urlConnection.setReadTimeout(readTimeout);
+            urlConnection.setRequestProperty("Accept", "application/json;");
+
+            buffer = new StringBuilder();
+            if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK)
+            {
+                bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+                while((readLine = bufferedReader.readLine()) != null)
+                {
+                    buffer.append(readLine).append("\n");
+                }
+            }
+            else
+            {
+                buffer.append("code : ");
+                buffer.append(urlConnection.getResponseCode()).append("\n");
+                buffer.append("message : ");
+                buffer.append(urlConnection.getResponseMessage()).append("\n");
+            }
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (bufferedWriter != null) { bufferedWriter.close(); }
+                if (bufferedReader != null) { bufferedReader.close(); }
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+
+
+        System.out.println("결과 : " + buffer.toString());
+    }
+
+    public static void main(String[] args) throws Exception{
+        weatherApi();
+    }
+
+}

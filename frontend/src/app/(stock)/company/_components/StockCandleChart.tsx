9e3443f16e366df29fa3a5e951b480bc204b1b9a
jsm0726 <jsm0726@naver.com>
2024-03-22 14:04:36 +0900

    [#49] feat: 주식 봉 차트 컴포넌트 생성 및 개별 회사 주식 화면에 연결

--- frontend/src/app/(stock)/company/_components/StockCandleChart.tsx
+++ frontend/src/app/(stock)/company/_components/StockCandleChart.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
-import { series } from './data';
+import { series, getMaxMinValueIndex } from './data';
 
 const datetime: 'datetime' = 'datetime';
 const pan: 'pan' = 'pan';
@@ -11,24 +11,113 @@ const option = {
   chart: {
     height: 290,
     id: 'candles',
+    fontFamily: 'pretendard, sans-serif',
     toolbar: {
       autoSelected: pan,
       show: false,
     },
     zoom: {
+      enabled: true,
+    },
+    animations: {
       enabled: false,
     },
   },
   plotOptions: {
     candlestick: {
       colors: {
-        upward: '#3C90EB',
-        downward: '#DF7D46',
+        upward: '#cd2626',
+        downward: '#5969ff',
       },
     },
   },
+  grid: {
+    show: true,
+  },
   xaxis: {
     type: datetime,
+    range: 1000 * 60 * 60 * 24 * 21,
+    labels: {
+      show: false,
+      format: 'dd일',
+      style: {
+        colors: '#d4d4d8',
+        fontSize: '10px',
+      },
+    },
+    axisBorder: { show: false },
+    axisTicks: { show: false },
+    tooltip: {
+      enabled: false,
+    },
+  },
+  yaxis: {
+    opposite: true,
+    show: true,
+    tickAmount: 3,
+    labels: {
+      style: {
+        colors: '#d4d4d8',
+        fontSize: '10px',
+      },
+      formatter: (value: number) => value.toFixed(0),
+      offsetX: -5,
+    },
+  },
+  annotations: {
+    yaxis: [
+      {
+        y: getMaxMinValueIndex(series)[0],
+        borderColor: '#cd2626',
+        label: {
+          borderColor: '#cd2626',
+          style: {
+            color: '#fff',
+            background: '#cd2626',
+          },
+          text: `최고: ${getMaxMinValueIndex(series)[0]}초코칩`,
+        },
+      },
+      {
+        y: getMaxMinValueIndex(series)[1],
+        borderColor: '#5969ff',
+        label: {
+          borderColor: '#5969ff',
+          style: {
+            color: '#fff',
+            background: '#5969ff',
+          },
+          text: `최저: ${getMaxMinValueIndex(series)[1]}초코칩`,
+          offsetY: 19,
+        },
+      },
+    ],
+  },
+
+  tooltip: {
+    custom: ({ w, dataPointIndex }: { w: any; dataPointIndex: number }) => {
+      return (
+        "<article class='tooltip'>" +
+        `<span class='tooltip-date'>${w.config.series[0].data[
+          dataPointIndex
+        ].x.getFullYear()}년 ${
+          w.config.series[0].data[dataPointIndex].x.getMonth() + 1
+        }월 ${w.config.series[0].data[dataPointIndex].x.getDate()}일</span>` +
+        `<span>시가: <strong class='tooltip-bold'>${w.config.series[0].data[
+          dataPointIndex
+        ].y[0].toFixed(0)}초코칩</strong></span>` +
+        `<span class='tooltip-blue'>저가: <strong class='tooltip-bold'>${w.config.series[0].data[
+          dataPointIndex
+        ].y[2].toFixed(0)}초코칩</strong></span>` +
+        `<span class='tooltip-red'>고가: <strong class='tooltip-bold'>${w.config.series[0].data[
+          dataPointIndex
+        ].y[1].toFixed(0)}초코칩</strong></span>` +
+        `<span>종가: <strong class='tooltip-bold'>${w.config.series[0].data[
+          dataPointIndex
+        ].y[3].toFixed(0)}초코칩</strong></span>` +
+        '</article>'
+      );
+    },
   },
 };
 
--- frontend/src/app/(stock)/company/_components/data.ts
+++ frontend/src/app/(stock)/company/_components/data.ts
@@ -1,246 +1,284 @@
+const timeStampArray = Array.from(
+  { length: 60 },
+  (_, i) =>
+    new Date(new Date('2024-01-23').getTime() + 1000 * 60 * 60 * 24 * i),
+);
+
 export const series = [
   {
     data: [
       {
-        x: new Date(1538778600000),
+        x: timeStampArray[0],
         y: [6629.81, 6650.5, 6623.04, 6633.33],
       },
       {
-        x: new Date(1538780400000),
+        x: timeStampArray[1],
         y: [6632.01, 6643.59, 6620, 6630.11],
       },
       {
-        x: new Date(1538782200000),
+        x: timeStampArray[2],
         y: [6630.71, 6648.95, 6623.34, 6635.65],
       },
       {
-        x: new Date(1538784000000),
+        x: timeStampArray[3],
         y: [6635.65, 6651, 6629.67, 6638.24],
       },
       {
-        x: new Date(1538785800000),
+        x: timeStampArray[4],
         y: [6638.24, 6640, 6620, 6624.47],
       },
       {
-        x: new Date(1538787600000),
+        x: timeStampArray[5],
         y: [6624.53, 6636.03, 6621.68, 6624.31],
       },
       {
-        x: new Date(1538789400000),
+        x: timeStampArray[6],
         y: [6624.61, 6632.2, 6617, 6626.02],
       },
       {
-        x: new Date(1538791200000),
+        x: timeStampArray[7],
         y: [6627, 6627.62, 6584.22, 6603.02],
       },
       {
-        x: new Date(1538793000000),
+        x: timeStampArray[8],
         y: [6605, 6608.03, 6598.95, 6604.01],
       },
       {
-        x: new Date(1538794800000),
+        x: timeStampArray[9],
         y: [6604.5, 6614.4, 6602.26, 6608.02],
       },
       {
-        x: new Date(1538796600000),
+        x: timeStampArray[10],
         y: [6608.02, 6610.68, 6601.99, 6608.91],
       },
       {
-        x: new Date(1538798400000),
+        x: timeStampArray[11],
         y: [6608.91, 6618.99, 6608.01, 6612],
       },
       {
-        x: new Date(1538800200000),
+        x: timeStampArray[12],
         y: [6612, 6615.13, 6605.09, 6612],
       },
       {
-        x: new Date(1538802000000),
+        x: timeStampArray[13],
         y: [6612, 6624.12, 6608.43, 6622.95],
       },
       {
-        x: new Date(1538803800000),
+        x: timeStampArray[14],
         y: [6623.91, 6623.91, 6615, 6615.67],
       },
       {
-        x: new Date(1538805600000),
+        x: timeStampArray[15],
         y: [6618.69, 6618.74, 6610, 6610.4],
       },
       {
-        x: new Date(1538807400000),
+        x: timeStampArray[16],
         y: [6611, 6622.78, 6610.4, 6614.9],
       },
       {
-        x: new Date(1538809200000),
+        x: timeStampArray[17],
         y: [6614.9, 6626.2, 6613.33, 6623.45],
       },
       {
-        x: new Date(1538811000000),
+        x: timeStampArray[18],
         y: [6623.48, 6627, 6618.38, 6620.35],
       },
       {
-        x: new Date(1538812800000),
+        x: timeStampArray[19],
         y: [6619.43, 6620.35, 6610.05, 6615.53],
       },
       {
-        x: new Date(1538814600000),
+        x: timeStampArray[20],
         y: [6615.53, 6617.93, 6610, 6615.19],
       },
       {
-        x: new Date(1538816400000),
+        x: timeStampArray[21],
         y: [6615.19, 6621.6, 6608.2, 6620],
       },
       {
-        x: new Date(1538818200000),
+        x: timeStampArray[22],
         y: [6619.54, 6625.17, 6614.15, 6620],
       },
       {
-        x: new Date(1538820000000),
+        x: timeStampArray[23],
         y: [6620.33, 6634.15, 6617.24, 6624.61],
       },
       {
-        x: new Date(1538821800000),
+        x: timeStampArray[24],
         y: [6625.95, 6626, 6611.66, 6617.58],
       },
       {
-        x: new Date(1538823600000),
+        x: timeStampArray[25],
         y: [6619, 6625.97, 6595.27, 6598.86],
       },
       {
-        x: new Date(1538825400000),
+        x: timeStampArray[26],
         y: [6598.86, 6598.88, 6570, 6587.16],
       },
       {
-        x: new Date(1538827200000),
+        x: timeStampArray[27],
         y: [6588.86, 6600, 6580, 6593.4],
       },
       {
-        x: new Date(1538829000000),
+        x: timeStampArray[28],
         y: [6593.99, 6598.89, 6585, 6587.81],
       },
       {
-        x: new Date(1538830800000),
+        x: timeStampArray[29],
         y: [6587.81, 6592.73, 6567.14, 6578],
       },
       {
-        x: new Date(1538832600000),
+        x: timeStampArray[30],
         y: [6578.35, 6581.72, 6567.39, 6579],
       },
       {
-        x: new Date(1538834400000),
+        x: timeStampArray[31],
         y: [6579.38, 6580.92, 6566.77, 6575.96],
       },
       {
-        x: new Date(1538836200000),
+        x: timeStampArray[32],
         y: [6575.96, 6589, 6571.77, 6588.92],
       },
       {
-        x: new Date(1538838000000),
+        x: timeStampArray[33],
         y: [6588.92, 6594, 6577.55, 6589.22],
       },
       {
-        x: new Date(1538839800000),
+        x: timeStampArray[34],
         y: [6589.3, 6598.89, 6589.1, 6596.08],
       },
       {
-        x: new Date(1538841600000),
+        x: timeStampArray[35],
         y: [6597.5, 6600, 6588.39, 6596.25],
       },
       {
-        x: new Date(1538843400000),
+        x: timeStampArray[36],
         y: [6598.03, 6600, 6588.73, 6595.97],
       },
       {
-        x: new Date(1538845200000),
+        x: timeStampArray[37],
         y: [6595.97, 6602.01, 6588.17, 6602],
       },
       {
-        x: new Date(1538847000000),
+        x: timeStampArray[38],
         y: [6602, 6607, 6596.51, 6599.95],
       },
       {
-        x: new Date(1538848800000),
+        x: timeStampArray[39],
         y: [6600.63, 6601.21, 6590.39, 6591.02],
       },
       {
-        x: new Date(1538850600000),
+        x: timeStampArray[40],
         y: [6591.02, 6603.08, 6591, 6591],
       },
       {
-        x: new Date(1538852400000),
+        x: timeStampArray[41],
         y: [6591, 6601.32, 6585, 6592],
       },
       {
-        x: new Date(1538854200000),
+        x: timeStampArray[42],
         y: [6593.13, 6596.01, 6590, 6593.34],
       },
       {
-        x: new Date(1538856000000),
+        x: timeStampArray[43],
         y: [6593.34, 6604.76, 6582.63, 6593.86],
       },
       {
-        x: new Date(1538857800000),
+        x: timeStampArray[44],
         y: [6593.86, 6604.28, 6586.57, 6600.01],
       },
       {
-        x: new Date(1538859600000),
+        x: timeStampArray[45],
         y: [6601.81, 6603.21, 6592.78, 6596.25],
       },
       {
-        x: new Date(1538861400000),
+        x: timeStampArray[46],
         y: [6596.25, 6604.2, 6590, 6602.99],
       },
       {
-        x: new Date(1538863200000),
+        x: timeStampArray[47],
         y: [6602.99, 6606, 6584.99, 6587.81],
       },
       {
-        x: new Date(1538865000000),
+        x: timeStampArray[48],
         y: [6587.81, 6595, 6583.27, 6591.96],
       },
       {
-        x: new Date(1538866800000),
+        x: timeStampArray[49],
         y: [6591.97, 6596.07, 6585, 6588.39],
       },
       {
-        x: new Date(1538868600000),
+        x: timeStampArray[50],
         y: [6587.6, 6598.21, 6587.6, 6594.27],
       },
       {
-        x: new Date(1538870400000),
+        x: timeStampArray[51],
         y: [6596.44, 6601, 6590, 6596.55],
       },
       {
-        x: new Date(1538872200000),
+        x: timeStampArray[52],
         y: [6598.91, 6605, 6596.61, 6600.02],
       },
       {
-        x: new Date(1538874000000),
+        x: timeStampArray[53],
         y: [6600.55, 6605, 6589.14, 6593.01],
       },
       {
-        x: new Date(1538875800000),
+        x: timeStampArray[54],
         y: [6593.15, 6605, 6592, 6603.06],
       },
       {
-        x: new Date(1538877600000),
+        x: timeStampArray[55],
         y: [6603.07, 6604.5, 6599.09, 6603.89],
       },
       {
-        x: new Date(1538879400000),
+        x: timeStampArray[56],
         y: [6604.44, 6604.44, 6600, 6603.5],
       },
       {
-        x: new Date(1538881200000),
+        x: timeStampArray[57],
         y: [6603.5, 6603.99, 6597.5, 6603.86],
       },
       {
-        x: new Date(1538883000000),
+        x: timeStampArray[58],
         y: [6603.85, 6605, 6600, 6604.07],
       },
       {
-        x: new Date(1538884800000),
+        x: timeStampArray[59],
         y: [6604.98, 6606, 6604.07, 6606],
       },
     ],
   },
 ];
+
+export const getMaxMinValueIndex = (series: any) => {
+  let maxValue: number = -1;
+  let minValue: number = -1;
+  let maxValueIndex: number | undefined = undefined;
+  let minValueIndex: number | undefined = undefined;
+
+  series[0].data.forEach((obj: any, index: number) => {
+    if (maxValue === -1 || minValue === -1) {
+      maxValue = obj.y[1];
+      minValue = obj.y[2];
+      maxValueIndex = index;
+      minValueIndex = index;
+    } else {
+      if (obj.y[1] > maxValue) {
+        maxValue = obj.y[1];
+        maxValueIndex = index;
+      }
+      if (obj.y[2] < minValue) {
+        minValue = obj.y[2];
+        minValueIndex = index;
+      }
+    }
+  });
+
+  return [
+    maxValue.toFixed(0),
+    minValue.toFixed(0),
+    maxValueIndex,
+    minValueIndex,
+  ];
+};

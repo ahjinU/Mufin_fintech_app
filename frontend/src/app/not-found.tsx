08aad22a7b0d8423e7c29da92f8a343758577b3d
jsm0726 <jsm0726@naver.com>
2024-03-25 09:01:27 +0900

    [#59] design: not-found 페이지 디자인 변경

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -7,13 +7,15 @@ import { Button } from '@/components';
 export default function NotFound() {
   return (
     <div className="relative w-[36rem] h-[64rem] pt-[5rem]">
-      <section className="w-[30rem] mx-auto flex flex-col gap-[1rem]">
-        <h3 className="custom-bold-text">페이지를 찾을 수 없습니다.</h3>
+      <section className="w-full mx-auto flex flex-col gap-[1rem]">
+        <h3 className="custom-bold-text ml-[1rem] mb-[1rem]">
+          페이지를 찾을 수 없습니다.
+        </h3>
         <Lottie
           loop
           animationData={lottieJson}
           play
-          style={{ width: 300, height: 300 }}
+          style={{ width: 360, height: 360 }}
         />
       </section>
 

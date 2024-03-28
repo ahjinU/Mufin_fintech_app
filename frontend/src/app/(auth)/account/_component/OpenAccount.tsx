37ad6f9413525542613b45575dc96d75eadcbdca
Seoan Yoon <earthdyoon@gmail.com>
2024-03-28 16:32:18 +0900

    [#61] design: 계좌 개설

--- frontend/src/app/(auth)/account/_component/OpenAccount.tsx
+++ frontend/src/app/(auth)/account/_component/OpenAccount.tsx
@@ -1,9 +1,13 @@
-
+import Image from 'next/image';
+import { GuideText } from '@/components';
 
 export default function OpenAccount() {
   return (
-    <div className="">
-      
+    <div className="flex flex-col gap-[1rem]">
+      <GuideText text="계좌를 개설해주세요" />
+      <div className="w-full h-[28.4rem] rounded-[2rem] bg-custom-light-gray">
+        {/* <Image src={} width={} height={} alt=''></Image> */}
+      </div>
     </div>
   );
 }

19b0880d12cc8f41f25a07685d0decdbc29688a2
Seoan Yoon <earthdyoon@gmail.com>
2024-03-30 16:02:30 +0900

    [#61] feat: 회원가입 유효성 검사 구현

--- frontend/src/app/(auth)/signup/_component/pagegroup/UserAccount.tsx
+++ frontend/src/app/(auth)/signup/_component/pagegroup/UserAccount.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { ComplexInput, Input, Button, TinyButton } from '@/components';
 import { checkEmailParent, checkEmailChild } from '../../_apis/apis';
+import { isValidEmail, isValidPassword } from '../../_utils/validator';
 
 export default function UserAccount({
   onNext,
@@ -10,24 +11,62 @@ export default function UserAccount({
 }) {
   const [account, setAccount] = useState({ email: '', password: '' });
   const [isValid, setIsValid] = useState(false);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState({
+    emailMessage: '',
+    passwordMessage: '',
+    checkMessage: '',
+  });
+  const [buttonMode, setButtonMode] = useState<'ACTIVE' | 'NON_ACTIVE'>(
+    'NON_ACTIVE',
+  );
 
   const onChangeInput = (e: { target: { name: string; value: string } }) => {
-    setAccount({ ...account, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setAccount({ ...account, [name]: value });
+
+    if (name === 'email') {
+      setMessage({
+        ...message,
+        emailMessage: isValidEmail(value)
+          ? ''
+          : '올바른 이메일 형식이 아니에요!',
+      });
+    } else if (name === 'password') {
+      setMessage({
+        ...message,
+        passwordMessage: isValidPassword(value)
+          ? ''
+          : '영문, 숫자, 특수문자를 포함해서 6~12자로 입력해주세요!',
+      });
+    } else if (name === 'password2') {
+      setMessage({
+        ...message,
+        checkMessage:
+          account.password == value ? '' : '비밀번호가 일치하지 않아요!',
+      });
+    }
   };
 
   const checkEmail = async () => {
-    try {
-      const fetchedData = await checkEmailParent(account.email);
-      if (fetchedData.ok) {
-        setIsValid(true);
-        setMessage('사용 가능한 이메일입니다😀');
-      } else {
-        setIsValid(false);
-        setMessage('중복된 이메일입니다😢');
+    if (isValidEmail(account.email)) {
+      try {
+        const fetchedData = await checkEmailParent(account.email);
+        if (fetchedData.ok) {
+          setIsValid(true);
+          setMessage({
+            ...message,
+            passwordMessage: '사용 가능한 이메일입니다😀',
+          });
+        } else {
+          setIsValid(false);
+          setMessage({
+            ...message,
+            passwordMessage: '중복된 이메일입니다😢',
+          });
+        }
+      } catch (error) {
+        console.error('이메일 중복 검사 에러', error);
       }
-    } catch (error) {
-      console.error('이메일 중복 검사 에러', error);
     }
   };
 
@@ -43,7 +82,7 @@ export default function UserAccount({
         label="이메일"
         mode={isValid ? 'INFORM' : 'ERROR'}
         isMsg
-        message={message}
+        message={message.emailMessage}
       >
         <div className="flex items-center gap-[1rem]">
           <Input
@@ -54,17 +93,27 @@ export default function UserAccount({
           <TinyButton label="중복 확인" onClick={checkEmail} />
         </div>
       </ComplexInput>
-      <ComplexInput label="비밀번호" mode="NONE">
+      <ComplexInput
+        label="비밀번호"
+        mode="ERROR"
+        isMsg
+        message={message.passwordMessage}
+      >
         <Input
           placeholder="비밀번호를 입력해주세요"
           name="password"
           onChange={onChangeInput}
         />
       </ComplexInput>
-      <ComplexInput label="비밀번호 확인" mode="NONE">
+      <ComplexInput
+        label="비밀번호 확인"
+        mode="ERROR"
+        isMsg
+        message={message.checkMessage}
+      >
         <Input
           placeholder="비밀번호를 한번 더 입력해주세요"
-          name="password"
+          name="password2"
           onChange={onChangeInput}
         />
       </ComplexInput>
---
+++ frontend/src/app/(auth)/signup/_utils/validator.ts
@@ -0,0 +1,35 @@
+export const isValidName = (name: string) => {
+  const nameRegex = /^[가-힣a-zA-Z]{2,6}$/;
+  return nameRegex.test(name);
+};
+
+export const isValidYear = (year: string): boolean => {
+  const yearRegex: RegExp = /^\d{4}$/;
+  return yearRegex.test(year);
+};
+
+export const isValidMonth = (month: string): boolean => {
+  const monthRegex: RegExp = /^(0[1-9]|1[0-2])$/;
+  return monthRegex.test(month);
+};
+
+export const isValidDay = (day: string): boolean => {
+  const dayRegex: RegExp = /^(0[1-9]|[1-2][0-9]|3[0-1])$/;
+  return dayRegex.test(day);
+};
+
+export const isValidPhoneNumber = (phoneNumber: string): boolean => {
+  const phoneRegex: RegExp = /^\d{11}$/;
+  return phoneRegex.test(phoneNumber);
+};
+
+export const isValidEmail = (email: string): boolean => {
+  const emailRegex: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+};
+
+export const isValidPassword = (password: string): boolean => {
+  const passwordRegex: RegExp =
+    /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,12}$/;
+  return passwordRegex.test(password);
+};

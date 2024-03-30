2eea6932db1c40a25c69333e453c351e45e8ae47
Seoan Yoon <earthdyoon@gmail.com>
2024-03-30 13:03:53 +0900

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const { data: session } = useSession();
 
   return (
-    <main>
+    <main className='relative'>
       <div className="min-h-screen flex flex-col items-center">
         {/* 여기 바꾸기 */}
         {!session ? <Main /> : <MainWithoutSignIn />}

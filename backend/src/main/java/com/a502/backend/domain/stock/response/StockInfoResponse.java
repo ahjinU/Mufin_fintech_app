518b831f244b682fbe305f901da5bbb3a74cb172
jep1995 <jep1995@naver.com>
2024-04-02 09:17:05 +0900

    [#26] chore: 주식 상세 조회 API 수정

--- backend/src/main/java/com/a502/backend/domain/stock/response/StockInfoResponse.java
+++ backend/src/main/java/com/a502/backend/domain/stock/response/StockInfoResponse.java
@@ -11,12 +11,16 @@ public class StockInfoResponse {
 	private double incomeRatio;
 	private int transCnt;
 	private String imageUrl;
+	private int upperLimitPrice;
+	private int lowerLimitPrice;
 
 	@Builder
-	public StockInfoResponse(int price, double incomeRatio, int transCnt, String imageUrl) {
+	public StockInfoResponse(int price, double incomeRatio, int transCnt, String imageUrl, int upperLimitPrice, int lowerLimitPrice) {
 		this.price = price;
 		this.incomeRatio = incomeRatio;
 		this.transCnt = transCnt;
 		this.imageUrl = imageUrl;
+		this.upperLimitPrice = upperLimitPrice;
+		this.lowerLimitPrice = lowerLimitPrice;
 	}
 }

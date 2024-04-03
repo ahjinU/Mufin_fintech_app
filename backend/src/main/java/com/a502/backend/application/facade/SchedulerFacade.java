476d26f4cec7c61614b57b7dc997b32470014962
slcloe <gkstmf1403@naver.com>
2024-04-03 09:31:38 +0900

    [#86] feat: 파킹통장 거래내역 이자 수정

--- backend/src/main/java/com/a502/backend/application/facade/SchedulerFacade.java
+++ backend/src/main/java/com/a502/backend/application/facade/SchedulerFacade.java
@@ -99,6 +99,8 @@ public class SchedulerFacade {
 
         for (Parking parking : parkingList) {
             int interest = (int) (parking.getBalance() * parking.getInterest() * 0.01);
+            interest /= 100;
+            interest *= 100;
             parkingService.updateParkingBalance(parking, interest + parking.getBalance());
             parkingDetailsService.saveInterest(parking, interest, codeService.findByName("이자"));
         }
